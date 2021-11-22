599c2080a9f09e1d7716de3716210a0a9ca13ba6
Andrei Lapets <a@lapets.io>
2021-11-21 22:12:17 -0500

    Add config, template, and build files for auto-generating docs; update README.

---
+++ docs/_source/b3q.rst
@@ -0,0 +1,8 @@
+b3q module
+==========
+
+
+.. automodule:: b3q.b3q
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 with open("README.rst", "r") as fh:
-    long_description = fh.read()
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+# The lines below can be parsed by `docs/conf.py`.
+name = "b3q"
+version = "0.1.1"
 
 setup(
-    name="b3q",
-    version="0.1.1",
-    packages=["b3q",],
+    name=name,
+    version=version,
+    packages=[name,],
     install_requires=[],
     license="MIT",
     url="https://github.com/nthparty/b3q",
